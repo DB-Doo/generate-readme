a18517c37cd5246c815e559934e24db1b3c8b1dc
Dan Brandt <dan.p.brandt@gmail.com>
2024-01-23 18:44:57 -0600

    imported modules to index.js so it correctly desctructures. Wrote renderLicenceLink function, rendeLlicenceBadge function. wrote function to pull in correct data for licence section on readme

--- develop/utils/generateMarkdown.js
+++ develop/utils/generateMarkdown.js
@@ -1,14 +1,42 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function to return the license badge link
+function renderLicenseBadge(license) {
+  // Check if the license is not provided or the user chose 'None' return an empty string
+  if (!license || license === 'None') return '';
+  // Object mapping each license to its corresponding badge image URL
+  const badgeLinks = {
+    'MIT': 'https://img.shields.io/badge/License-MIT-yellow.svg',
+    'GNU GPLv3': 'https://img.shields.io/badge/License-GPLv3-blue.svg',
+    'Apache 2.0': 'https://img.shields.io/badge/License-Apache%202.0-blue.svg',
+    'BSD 3-Clause': 'https://img.shields.io/badge/License-BSD%203--Clause-blue.svg'
+  };
+  // Returns a string for the license badge image.
+  // which is then selected based on the license chosen by the user
+  return `![License: ${license}](${badgeLinks[license]})`;
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function to return the license link
+function renderLicenseLink(license) {
+  // Check if the license is not provided or the user chose 'None' return an empty string
+  if (!license || license === 'None') return '';
+  // Object mapping each license to its corresponding official URL for more details
+  const licenseLinks = {
+    'MIT': 'https://opensource.org/licenses/MIT',
+    'GNU GPLv3': 'https://www.gnu.org/licenses/gpl-3.0',
+    'Apache 2.0': 'https://opensource.org/licenses/Apache-2.0',
+    'BSD 3-Clause': 'https://opensource.org/licenses/BSD-3-Clause'
+  };
+  // returns a string links to the official license page.
+  // enabling users to read more about their selected license
+  return `View more about ${license} license [here](${licenseLinks[license]})`;
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function to return the license section of README
+function renderLicenseSection(license) {
+  if (!license || license === 'None') return '';
+  return `## License
+  
+This project is licensed under the ${license} License - see the [LICENSE.md](LICENSE.md) file for details`;
+}
 
 // TODO: Create a function to generate markdown for README
 // takes the users input data and generates the markdown text for the README
@@ -51,4 +79,9 @@ For any questions, please contact me at:
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = {
+  generateMarkdown,
+  renderLicenseBadge,
+  renderLicenseLink,
+  renderLicenseSection
+}
